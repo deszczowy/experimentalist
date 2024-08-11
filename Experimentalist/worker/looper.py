41a8353bd91de1ca05bebc0f33de443b7bfe1e51
deszczowy <krystian.szklarek@op.pl>
2024-08-11 14:56:24 +0200

    style for workers

--- Experimentalist/worker/looper.py
+++ Experimentalist/worker/looper.py
@@ -1,33 +1,42 @@
 from Experimentalist.Actions import Fade, Loop, Stretch
 from Experimentalist.Core import Worker
 
+
 class Looper(Worker):
 
-    def __init__(self, filePath, maxDuration : float = 10):
+    def __init__(self, filePath: str, maxDuration: float = 10) -> None:
         super().__init__(filePath, "Looper")
         self.maxDuration = maxDuration
-        
-    def process(self):
+
+    def process(self) -> None:
 
         if self.length > self.maxDuration:
             while True:
                 self.audio = Fade().process(self.audio, self.sample_rate)
                 self.audio = Loop().process(self.audio, self.sample_rate)
                 super().process()
-            
+
                 if self.length < self.maxDuration:
                     break
-        
+
+
 class Drone(Worker):
 
-    def __init__(self, filePath, factor : float = 0.9, maxDuration : float = 10):
+    def __init__(
+            self,
+            filePath: str,
+            factor: float = 0.9,
+            maxDuration: float = 10
+            ) -> None:
         super().__init__(filePath, "Drone")
         self.maxDuration = maxDuration
         self.factor = factor
 
-    def process(self):
+    def process(self) -> None:
         while True:
-            self.audio = Stretch(self.factor).process(self.audio, self.sample_rate)
+            self.audio = Stretch(self.factor).process(
+                self.audio,
+                self.sample_rate)
             self.audio = Fade().process(self.audio, self.sample_rate)
             self.audio = Loop().process(self.audio, self.sample_rate)
             super().process()
