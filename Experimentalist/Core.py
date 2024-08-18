4c9120e2da5f7bb0a6bbdf209ccd284d296b9466
deszczowy <krystian.szklarek@op.pl>
2024-08-18 21:23:42 +0200

    proper module, i guess..

--- Experimentalist/Core.py
+++
@@ -1,5 +0,0 @@
-from Experimentalist.core.log import Log  # noqa
-from Experimentalist.core.action import Action  # noqa
-from Experimentalist.core.args import Arguments  # noqa
-from Experimentalist.core.processor import Processor  # noqa
-from Experimentalist.core.worker import Worker  # noqa
---
+++ Experimentalist/Core/__init__.py
@@ -0,0 +1,5 @@
+from Experimentalist.Core.log import Log  # noqa
+from Experimentalist.Core.action import Action  # noqa
+from Experimentalist.Core.args import Arguments  # noqa
+from Experimentalist.Core.processor import Processor  # noqa
+from Experimentalist.Core.worker import Worker  # noqa
