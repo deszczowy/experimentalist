5582d9119cfc78de8344558f9eeee1e0d48e25d7
deszczowy <krystian.szklarek@op.pl>
2024-08-18 21:34:05 +0200

    proper workers module

--- Experimentalist/Workers.py
+++
@@ -1,3 +0,0 @@
-from Experimentalist.worker.looper import Looper  # noqa
-from Experimentalist.worker.droner import Droner  # noqa
-from Experimentalist.worker.spacer import Spacer  # noqa
---
+++ Experimentalist/Workers/__init__.py
@@ -0,0 +1,3 @@
+from Experimentalist.Workers.looper import Looper  # noqa
+from Experimentalist.Workers.droner import Droner  # noqa
+from Experimentalist.Workers.spacer import Spacer  # noqa
