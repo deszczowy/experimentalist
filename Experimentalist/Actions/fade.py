7cf2c6aba81677501f9ed49782fb8c4ebdd17070
deszczowy <krystian.szklarek@op.pl>
2025-12-12 18:05:49 +0100

    fade action is now based on Audio class

--- Experimentalist/Actions/fade.py
+++ Experimentalist/Actions/fade.py
@@ -1,4 +1,4 @@
-from Experimentalist.Core import Action
+from Experimentalist.Core import Audio, Action
 import numpy as np
 
 
@@ -8,25 +8,21 @@ class Fade(Action):
         super().__init__("Fade")
         self.peak = np.clip(peak_position, 0, 1)
 
-    def process(self, audio: np.ndarray, sample_rate: float) -> np.ndarray:
-        super().process(audio, sample_rate)
-
-        peak_point = int(self.length * self.peak)
+    def process(self, audio: Audio) -> None:
+        peak_point = int(audio.count * self.peak)
 
         if peak_point == 0:
             peak_point += 1
-        if peak_point == self.length:
+        if peak_point == audio.count:
             peak_point -= 1
 
         coef1 = 1 / peak_point  # fade in
-        coef2 = 1 / (self.length - peak_point)  # fade out
+        coef2 = 1 / (audio.count - peak_point)  # fade out
 
         for i in range(0, peak_point - 1):
-            audio[i, 0] = audio[i, 0] * coef1 * i
-            audio[i, 1] = audio[i, 1] * coef1 * i
+            audio.frames[i, 0] = audio.frames[i, 0] * coef1 * i
+            audio.frames[i, 1] = audio.frames[i, 1] * coef1 * i
 
-        for i in range(peak_point, self.length):
-            audio[i, 0] = audio[i, 0] * coef2 * (self.length - i)
-            audio[i, 1] = audio[i, 1] * coef2 * (self.length - i)
-
-        return audio
+        for i in range(peak_point, audio.count):
+            audio.frames[i, 0] = audio.frames[i, 0] * coef2 * (audio.count - i)
+            audio.frames[i, 1] = audio.frames[i, 1] * coef2 * (audio.count - i)
