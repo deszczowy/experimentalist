7dd08f13d136d26e7de310bef4b7eddb0dcc752f
deszczowy <krystian.szklarek@op.pl>
2025-12-13 19:13:12 +0100

    high pass filter action is now based on Audio class

--- Experimentalist/Actions/highpass.py
+++ Experimentalist/Actions/highpass.py
@@ -1,16 +1,24 @@
-from Experimentalist.Core import Action
+from Experimentalist.Core import Audio, Action
 import numpy as np
 from pedalboard import HighpassFilter
 
 
 class HighPass(Action):
+    """
+    High pass filter action.
+
+    Parameters
+    ----------
+
+    cutoff_hz: float
+        Filter will pass frequencies above this value. Default is 10kHz.
+
+    """
 
     def __init__(self, cutoff_hz: float = 10000) -> None:
         super().__init__("High Pass")
         self.cutoff = cutoff_hz
 
-    def process(self, audio: np.ndarray, sample_rate: float) -> np.ndarray:
-        audio = HighpassFilter(
-            cutoff_frequency_hz=self.cutoff).process(audio, sample_rate)
-        super().process(audio, sample_rate)
-        return audio
+    def process(self, audio: Audio) -> None:
+        audio.frames = HighpassFilter(
+            cutoff_frequency_hz=self.cutoff).process(audio.frames, audio.sample_rate)
