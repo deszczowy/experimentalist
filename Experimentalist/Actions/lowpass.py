7281eb6b53818d417724bae7f85a91766b068c45
deszczowy <krystian.szklarek@op.pl>
2025-12-13 19:39:33 +0100

    low pass filter action is now based on Audio class

--- Experimentalist/Actions/lowpass.py
+++ Experimentalist/Actions/lowpass.py
@@ -1,16 +1,23 @@
-from Experimentalist.Core import Action
+from Experimentalist.Core import Audio, Action
 import numpy as np
 from pedalboard import LowpassFilter
 
 
 class LowPass(Action):
+    """
+    Low pass filter action.
 
+    Parameters
+    ----------
+
+    cutoff_hz: float
+        Filter will pass frequencies below this value. Default is 100Hz.
+
+    """
     def __init__(self, cutoff_hz: float = 100) -> None:
         super().__init__("Low Pass")
         self.cutoff = cutoff_hz
 
-    def process(self, audio: np.ndarray, sample_rate: float) -> np.ndarray:
-        audio = LowpassFilter(
-            cutoff_frequency_hz=self.cutoff).process(audio, sample_rate)
-        super().process(audio, sample_rate)
-        return audio
+    def process(self, audio: Audio) -> None:
+        audio.frames = LowpassFilter(
+            cutoff_frequency_hz=self.cutoff).process(audio.frames, audio.sample_rate)
